41e8730568b11bc4994a1eae64b91e160853b2e3
khatrisagar <khatrisagar404@gmail.com>
2023-04-10 11:58:36 +0530

    issue fixed

--- public/script/checkin.js
+++ public/script/checkin.js
@@ -25,7 +25,6 @@ const checkIn = async() => {
     document.getElementById("checkout").classList.toggle("btn-checkout1");
 
     document.getElementById("green").innerHTML += ` <div class="check-green">
-    <span ><label>Checked In :</label>${moment(data.checkindate).utcOffset(tz).format("hh:mm:ss")}  </span>
     </div>`
 
 }
